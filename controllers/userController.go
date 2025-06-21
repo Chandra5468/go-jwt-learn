66a824c5d593fece6706bb0aefa8ea42ed5c09e2
Chandra5468 <agirchandra@gmail.com>
2025-06-21 22:35:15 +0530

    gin and create jwt token created

--- controllers/userController.go
+++ controllers/userController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -9,14 +10,69 @@ import (
 	"github.com/Chandra5468/go-jwt-learn/helpers"
 	"github.com/Chandra5468/go-jwt-learn/models"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 var userCollection *mongo.Collection = mango.OpenCollection(mango.MongoCon, "user")
+var validate = validator.New()
 
 func Signup() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		ctx2, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		var user models.User
+
+		if err := ctx.BindJSON(&user); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(&user)
+
+		if validationErr != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		countEmail, err := userCollection.CountDocuments(ctx2, bson.M{"email": user.Email})
+
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		countPhone, err := userCollection.CountDocuments(ctx2, bson.M{"phone": user.Phone})
+
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		if countEmail > 0 || countPhone > 0 {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "email or phonenumber already exists"})
+			return
+		}
+
+		user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		_, err = userCollection.InsertOne(ctx2, &user)
+		if err != nil {
+			msg := fmt.Sprintf("User item is not created %s", err.Error())
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		// uid := res.InsertedID.(primitive.ObjectID)
+		// THis should be mongo inserted document
+		// user.User_id = math.rand()  //uid.Hex()
+		token, refreshToken, _ := helpers.GenerateAllTokens(&user.Email, &user.FirstName, &user.LastName, &user.UserType)
+
+		user.Token = token
+		user.RefreshToken = refreshToken
+
+		ctx.JSON(http.StatusCreated, user.User_id)
 
 	}
 }
--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,1 +1,52 @@
 package helpers
+
+import (
+	"log"
+	"os"
+	"time"
+
+	mango "github.com/Chandra5468/go-jwt-learn/database/Mango"
+	"github.com/golang-jwt/jwt/v5"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+// type SignedDetails struct {
+// 	Email     string
+// 	FirstName string
+// 	LastName  string
+// 	UserType  string
+// 	jwt.Claims
+// }
+
+var userCollection *mongo.Collection = mango.OpenCollection(mango.MongoCon, "user")
+
+var SECRET string = os.Getenv("SECRET")
+
+func GenerateAllTokens(Email, FirstName, LastName, UserType *string) (signedToken, refreshToken string, err error) {
+	// cms := &SignedDetails{
+	// 	Email:      *Email,
+	// 	FirstName:  *FirstName,
+	// 	LastName:   *LastName,
+	// 	UserType:   *UserType,
+	// 	jwt.Claims: jwt.Claims{},
+	// }
+
+	claims := jwt.MapClaims{
+		"Email":     *Email,
+		"FirstName": *FirstName,
+		"LastName":  *LastName,
+		"UserType":  *UserType,
+		"iat":       time.Now().Unix(),
+		"exp":       time.Now().Add(time.Hour * 3).Unix(), // expiration time after 3 hours
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	secret := os.Getenv("SECRET")
+	signedToken, err = token.SignedString(secret)
+
+	if err != nil {
+		log.Printf("Error in generating signed token for loggedin/registered users %s", err.Error())
+		return "", "", err
+	}
+
+}
